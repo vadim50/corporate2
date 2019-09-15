720467e860ef652da916b210adbba5c02c8308b4
vadim <vadim50>
2019-09-15 17:54:12 +0300

    Lesson 57(Маршруты.Глобальный контроллер пользовательской части)

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/','IndexController',[
+										'only'=>['index'],
+
+										'names'=>['index'=>'home1']
+
+									]);
